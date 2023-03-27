346491745182978f114fa5c0bb009ade83d94c7e
Mahbub <mahbub.095@gmail.com>
2023-03-28 00:04:52 +0600

    core concept

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const nayoks = ['Razzak', 'Salman', 'Sakib', 'Shuvo']
+  const products = [
+    {name: 'Photoshop', price:'$90.99'},
+    {name: 'Illustrator', price:'$60.99'},
+    {name: 'PDF Reader', price: '$6.99'},
+    {name: 'Premiere El', price: '$249.99'}
+  ]
+
+  const nayokNames = nayoks.map(nayok =>nayok)
+  console.log(nayokNames);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>I am React Man</p>
+       <ul>
+        <li>{nayoks[0]}</li>
+        <li>{nayoks[1]}</li>
+        <li>{nayoks[2]}</li>
+        <li>{nayoks[3]}</li>
+       </ul>
+      <products product = {products[0]}></products> 
       </header>
     </div>
   );
